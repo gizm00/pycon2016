8e66c5e42d268255a40a4877d9d95ce9bd48b175
Sev Leonard <jennifer.m.leonard@gmail.com>
2016-05-23 16:26:26 -0700

    updating start-notebook.sh

--- docker/start-notebook.sh
+++ docker/start-notebook.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# Install mysql driver
+exec git clone https://github.com/mysql/mysql-connector-python.git /tmp/mysql-connector-python
+exec cd /tmp/mysql-connector-python
+exec python ./setup.py build
+exec sudo ./setup.py install
 
 exec jupyter notebook &> /dev/null &
 
